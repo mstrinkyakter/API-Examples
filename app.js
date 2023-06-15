66ebd8f6a29afa47131c8c2af0140b52995f327c
Mst Rinky Akter <mstrinkya@gmail.com>
2023-06-15 23:27:11 +0600

    ///

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const loadPhones = async() =>{
- const url='https://openapi.programming-hero.com/api/phones?search=iphone'
+const loadPhones = async(searchText) =>{
+ const url='https://openapi.programming-hero.com/api/phones?search=${searchText}'
  const res= await fetch(url);
  const data= await res.json();
  displayPhones(data.data);
@@ -7,11 +7,12 @@ const loadPhones = async() =>{
 const displayPhones = phones => {
     // console.log(phones);
     const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
     phones.forEach(phone =>{
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML =`
-        <div class="card">
+        <div class="card p-4">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
@@ -24,6 +25,9 @@ const displayPhones = phones => {
 }
 
 document.getElementById('btn-search').addEventListener('click',function(){
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
     
 })
  
